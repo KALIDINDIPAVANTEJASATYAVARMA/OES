c418f80b920429b2fd3528a6ca7541cee1f254c3
KALIDINDI PAVAN TEJA SATYA VARMA <kalidindipavantejasatyavarma@gmail.com>
2023-05-17 21:17:09 +0530

    initial commit

--- exam/forms.py
+++ exam/forms.py
@@ -2,10 +2,6 @@ from django import forms
 from django.contrib.auth.models import User
 from . import models
 
-class ContactusForm(forms.Form):
-    Name = forms.CharField(max_length=30)
-    Email = forms.EmailField()
-    Message = forms.CharField(max_length=500,widget=forms.Textarea(attrs={'rows': 3, 'cols': 30}))
 
 class TeacherSalaryForm(forms.Form):
     salary=forms.IntegerField()
@@ -17,8 +13,7 @@ class CourseForm(forms.ModelForm):
 
 class QuestionForm(forms.ModelForm):
     
-    #this will show dropdown __str__ method course model is shown on html so override it
-    #to_field_name this will fetch corresponding value  user_id present in course model and return it
+    
     courseID=forms.ModelChoiceField(queryset=models.Course.objects.all(),empty_label="Course Name", to_field_name="id")
     class Meta:
         model=models.Question
